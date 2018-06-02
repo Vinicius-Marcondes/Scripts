b09585f8d98ef388e6eef328e7442fa32b890c61
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2018-06-02 16:15:15 -0300

    Now the final version

--- git_ssh_configuration.sh
+++ git_ssh_configuration.sh
@@ -3,20 +3,23 @@ sudo apt update
 sudo apt install git
 sudo apt install xclip
 echo "Insert your email"
-read email
+read email;
 echo "Confirm your email: $email (Y/N)"
-read id
-if [ "$id" == "Y" || "$id" == "y" ];
-then
-	echo "$id"	
-elif [ "$id" == "N" || "$id" == "n" ];
-then
-	echo "Insert your email"
-	read email
-	echo "Confirm your email: $email (S/N)"
-	read id
+read id;
+if [ $id == "Y" ];
+	then
+		echo "Sua opção foi: $id"	
+elif [ $id == "N" ];
+	then
+		echo "Insert your email"
+		read email;
+		echo "Confirm your email: $email (S/N)"
+		read id;
 fi
-ssh-keygen -t rsa -b 4096 -C "$email"
-ssh -add ~/.ssh/id_rsa
-xclip -sel clip < ~/.ssh/id_rsa.pub
+if [ $id == "Y" ];
+then 
+	ssh-keygen -t rsa -b 4096 -C "$email"
+	ssh -add ~/.ssh/id_rsa
+	xclip -sel clip < ~/.ssh/id_rsa.pub
+fi
 echo "Done!"
