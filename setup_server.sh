0d4d532c774e517976c012929c5f28afd8b809d0
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2018-06-03 22:32:55 -0300

    added phpmyadmin

--- setup_server.sh
+++ setup_server.sh
@@ -7,3 +7,4 @@ sudo apt install php libapache2-mod-php php-mysql
 sudo vi /etc/apache2/mods-enabled/dir.conf
 sudo systemctl restart apache2
 sudo apt install php-cli
+sudo apt install phpmyadmin php-gettext
